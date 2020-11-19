de24d750cbf66b6cd9c6800825dfa14be830285e
Pedro Moleiro <pedro.r.p.moleiro@protonmail.com>
2020-11-19 20:30:13 +0000

    Laravel Passport Configured

--- app/Http/Controllers/LoginControllerAPI.php
+++ app/Http/Controllers/LoginControllerAPI.php
@@ -10,14 +10,30 @@ class LoginControllerAPI extends Controller
     {
         $http = new \GuzzleHttp\Client;
 
-        $response = $http->post(env("APP_URL") . '/oauth/token', ['form_params' => ['grant_type' => 'password', 'client_id' => env("CLIENT_ID"), 'client_secret' => env("CLIENT_SECRET"), 'username' => $request->email, 'password' => $request->password, 'scope' => ''], 'exceptions' => false,]);
+        $response = $http->post( env('APP_URL').'/oauth/token', [
+            'form_params' => [
+                'grant_type' => 'password',
+                'client_id' => env('PASSPORT_ID'),
+                'client_secret' => env('PASSPORT_SECRET'),
+                'username' => $request->email,
+                'password' => $request->password,
+                'scope' => ''
+            ],
+            'exceptions' => false,
+        ]);
 
         $errorCode = $response->getStatusCode();
-
         if ($errorCode == '200') {
             return json_decode((string) $response->getBody(), true);
         } else {
             return response()->json(['msg' => 'User credentials are invalid'], $errorCode);
         }
+    }
+
+    public function logout()
+    {
+        \Auth::guard('api')->user()->token()->revoke();
+        \Auth::guard('api')->user()->token()->delete();
+        return response()->json(['msg' => 'Token revoked'], 200);
     }
 }
