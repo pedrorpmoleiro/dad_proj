92609ca1d679b850d1eb7306224e4c27c8f080cd
Pedro Moleiro <pedro.r.p.moleiro@protonmail.com>
2020-12-26 20:46:43 +0000

    Prod Build

--- resources/js/components/pages/ManageAccounts.vue
+++ resources/js/components/pages/ManageAccounts.vue
@@ -1,104 +1,103 @@
 <template>
-    <div>
-        <v-container>
+    <v-container>
+        <v-card flat>
+            <v-toolbar
+                rounded
+                flat
+            >
+                <v-toolbar-title>Manager Dashboard</v-toolbar-title>
+                <v-divider
+                    class="mx-4"
+                    inset
+                    vertical
+                ></v-divider>
+                <v-spacer></v-spacer>
+                <v-dialog
+                    v-model="dialog"
+                    max-width="500px"
+                >
+                    <v-card>
+                        <v-card-title>
+                            <span class="headline">{{ formTitle }}</span>
+                        </v-card-title>
+
+                        <v-card-text>
+                            <v-container>
+                                <v-row>
+                                    <v-col
+                                        cols="12"
+                                        sm="6"
+                                        md="4"
+                                    >
+                                        <v-text-field
+                                            v-model="editedItem.name"
+                                            label="Name"
+                                        ></v-text-field>
+                                    </v-col>
+                                    <v-col
+                                        cols="12"
+                                        sm="6"
+                                        md="4"
+                                    >
+                                        <v-text-field
+                                            v-model="editedItem.email"
+                                            label="Email"
+                                        ></v-text-field>
+                                    </v-col>
+                                    <v-col
+                                        cols="12"
+                                        sm="6"
+                                        md="4"
+                                    >
+                                        <v-autocomplete
+                                            clearable
+                                            flat
+                                            v-model="editedItem.type"
+                                            label="Type"
+                                            :items="userTypes"
+                                        ></v-autocomplete>
+                                    </v-col>
+                                </v-row>
+                            </v-container>
+                        </v-card-text>
+
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn
+                                color="red darken-1"
+                                text
+                                @click="close"
+                            >
+                                Cancel
+                            </v-btn>
+                            <v-btn
+                                color="green darken-1"
+                                text
+                                @click="save"
+                            >
+                                Save
+                            </v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-dialog>
+                <v-dialog v-model="dialogDelete" max-width="500px">
+                    <v-card>
+                        <v-card-title class="headline">Confirm delete action</v-card-title>
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn color="red darken-1" text @click="closeDelete">Cancel</v-btn>
+                            <v-btn color="green darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                            <v-spacer></v-spacer>
+                        </v-card-actions>
+                    </v-card>
+                </v-dialog>
+            </v-toolbar>
             <v-data-table
                 :headers="headers"
                 :items="users"
                 sort-by="id"
                 class="elevation-1"
             >
-                <template v-slot:top>
-                    <v-toolbar
-                        flat
-                    >
-                        <v-toolbar-title>Manager Dashboard</v-toolbar-title>
-                        <v-divider
-                            class="mx-4"
-                            inset
-                            vertical
-                        ></v-divider>
-                        <v-spacer></v-spacer>
-                        <v-dialog
-                            v-model="dialog"
-                            max-width="500px"
-                        >
-                            <v-card>
-                                <v-card-title>
-                                    <span class="headline">{{ formTitle }}</span>
-                                </v-card-title>
-
-                                <v-card-text>
-                                    <v-container>
-                                        <v-row>
-                                            <v-col
-                                                cols="12"
-                                                sm="6"
-                                                md="4"
-                                            >
-                                                <v-text-field
-                                                    v-model="editedItem.name"
-                                                    label="Name"
-                                                ></v-text-field>
-                                            </v-col>
-                                            <v-col
-                                                cols="12"
-                                                sm="6"
-                                                md="4"
-                                            >
-                                                <v-text-field
-                                                    v-model="editedItem.email"
-                                                    label="Email"
-                                                ></v-text-field>
-                                            </v-col>
-                                            <v-col
-                                                cols="12"
-                                                sm="6"
-                                                md="4"
-                                            >
-                                                <v-autocomplete
-                                                    clearable
-                                                    flat
-                                                    v-model="editedItem.type"
-                                                    label="Type"
-                                                    :items="userTypes"
-                                                ></v-autocomplete>
-                                            </v-col>
-                                        </v-row>
-                                    </v-container>
-                                </v-card-text>
-
-                                <v-card-actions>
-                                    <v-spacer></v-spacer>
-                                    <v-btn
-                                        color="red darken-1"
-                                        text
-                                        @click="close"
-                                    >
-                                        Cancel
-                                    </v-btn>
-                                    <v-btn
-                                        color="green darken-1"
-                                        text
-                                        @click="save"
-                                    >
-                                        Save
-                                    </v-btn>
-                                </v-card-actions>
-                            </v-card>
-                        </v-dialog>
-                        <v-dialog v-model="dialogDelete" max-width="500px">
-                            <v-card>
-                                <v-card-title class="headline">Confirm delete action</v-card-title>
-                                <v-card-actions>
-                                    <v-spacer></v-spacer>
-                                    <v-btn color="red darken-1" text @click="closeDelete">Cancel</v-btn>
-                                    <v-btn color="green darken-1" text @click="deleteItemConfirm">OK</v-btn>
-                                    <v-spacer></v-spacer>
-                                </v-card-actions>
-                            </v-card>
-                        </v-dialog>
-                    </v-toolbar>
-                </template>
                 <template v-slot:item.actions="{ item }">
                     <v-icon
                         small
@@ -123,8 +122,8 @@
                     </v-btn>
                 </template>
             </v-data-table>
-        </v-container>
-    </div>
+        </v-card>
+    </v-container>
 </template>
 
 <script>
